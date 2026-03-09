518928170006af685ba4247f4136be541de6c5f3
MakovhaThendo <leonthendo99@gmail.com>
2026-03-09 11:46:45 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // Register User
 exports.registerUser = async (req, res) => {

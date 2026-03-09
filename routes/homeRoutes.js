7fe09e637ce487cbf716691969638e99ba474295
MakovhaThendo <leonthendo99@gmail.com>
2026-03-09 11:52:49 +0200

    Update homeRoutes.js

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const { registerUser } = require("../controllers/authController");
-const User = require("../models/User");
+const User = require("../models/user");
 
 //  check if server is running
 router.get("/", (req, res) => {
